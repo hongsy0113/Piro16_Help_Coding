282eb71db56bc6f7f686634650d889ed611d728e
Yoon <yoonk0212@ewhain.net>
2022-02-03 15:58:58 +0900

    [UPDATE] user, reward model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
+class Job(models.Model):
+    school_company = models.CharField(max_length=50)
+    
+class User(models.Model):
+    # 장고 모델 상속하는 걸로 해야 함
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+    birth = models.DateField(null=True, blank=True)
+    img = models.ImageField(upload_to="image/", null=True, blank=True)
+    introduction = models.TextField(null=True, blank=True)
+    total_like = models.IntegerField(null=True, blank=True)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
+class Reward(models.Model):
+    name = models.CharField(max_length=20)
+    info = models.TextField()
+    img = models.ImageField(upload_to="image/", null=True, blank=True)
+
+    def __str__(self):
+        return self.id
+
+
+class GetReward(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    reward_id =  models.ForeignKey(Reward, on_delete=models.CASCADE)
+    get_date = models.DateTimeField()
+
+class Alert(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+    alert_type = models.IntegerField()
+    time = models.DateTimeField()
