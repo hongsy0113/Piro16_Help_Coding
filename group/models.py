513d4cb448bf955d3fcb43028d5d1afe2707eae4
Yejun Park <jun020216@naver.com>
2022-02-03 16:09:23 +0900

    [FIX] group model

--- group/models.py
+++ group/models.py
@@ -7,6 +7,12 @@ class Group(models.Model):
     maker = models.CharField(verbose_name="방장")
     code = models.CharField(verbose_name="초대 코드", max_length=6, null=True)  #랜덤 코드 길이 설정
 
+    def __str__(self):
+        return self.id
+
 class Participation(models.Model):
     group = models.ForeignKey(Group, verbose_name="소속 그룹명", blank=True, on_delete=models.CASCADE)
     member = models.ForeignKey(User, verbose_name="그룹멤버")
+
+    def __str__(self):
+        return self.id
