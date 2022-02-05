7fca743ac39c32f12c48f7245a9017e4201d56a3
PARK CHANYOUNG <pcy1203@snu.ac.kr>
2022-02-06 01:31:40 +0900

    [FEAT] user mypage (profile, reward, question, answer)

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
   path('login/', views.LoginView.as_view(), name="login"),
   path('signup/', views.sign_up, name="signup"),
   path('logout/', views.log_out, name="logout"),
+  path('mypage/', views.my_page, name="mypage")
 ]
--- user/views.py
+++ user/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from .forms import LoginForm, SignupForm
-from .models import User
+from .models import *
+from qna.models import Question, Answer
 
 def main(request):
     return render(request, 'user/main.html')
@@ -10,20 +11,20 @@ def main(request):
 # Login
 class LoginView(View):
     def post(self, request):
-      form = LoginForm(request.POST)
-      if form.is_valid():
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        user = authenticate(request, username = email, password = password)
-        if user is not None:
-          login(request, user)
-          return redirect('user:main')
-      ctx = {'form': form}
-      return render(request, 'user/login.html', ctx)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username = email, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('user:main')
+        ctx = {'form': form}
+        return render(request, 'user/login.html', ctx)
 
     def get(self, request):
-      form = LoginForm()
-      return render(request, 'user/login.html', {'form': form})
+        form = LoginForm()
+        return render(request, 'user/login.html', {'form': form})
 
 # Logout
 def log_out(request):
@@ -48,28 +49,13 @@ def sign_up(request):
             return redirect('user:main')
     else:
         form = SignupForm()
-        return render(request, 'user/signup.html', {"form": form})
+        return render(request, 'user/signup.html', {'form': form})
 
-
-# Sign Up
-'''
-def sign_up(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                username = request.POST['email'],
-                email = request.POST['email'],
-                password = request.POST['password1'],
-                nickname = request.POST['nickname'],
-                birth = request.POST['birth'],
-                img = request.POST['img'],
-                introduction = request.POST['introduction'],
-                total_like = 0,
-                job = request.POST['job'],
-            )
-            login(request, user)
-            return redirect('user:main')
-    else:
-        form = SignupForm()
-        return render(request, 'user/signup.html', {"form": form})
-'''
+# My Page
+def my_page(request):
+    user = request.user
+    rewards = GetReward.objects.filter(user_id = user).order_by('-get_date')[:5]
+    questions = Question.objects.filter(user = user).order_by('-updated_at')[:5]
+    answers = Answer.objects.filter(user = user).order_by('-updated_at')[:5]
+    ctx = {'user': user, 'rewards': rewards, 'questions': questions, 'answers': answers}
+    return render(request, template_name='user/mypage.html', context = ctx)
