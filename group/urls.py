26bb6a53f84fe1910fe6db6b021a709e962e5b04
Yejun Park <jun020216@naver.com>
2022-02-06 01:42:50 +0900

    [FEAT] 그룹 초대&가입

--- group/urls.py
+++ group/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#app_name = 'group'
+
 urlpatterns = [
     path('', view=views.group_home, name='group_home'),
     path('group_create/', view=views.group_create, name='group_create'),
@@ -8,6 +10,8 @@ urlpatterns = [
     path('<int:pk>/group_delete/', view=views.group_delete, name='group_delete'),
     path('<int:pk>/group_detail/', view=views.group_detail, name='group_detail' ),
     path('<int:pk>/create_code/', view=views.create_code, name='create_code'),
-    # path('join_group/', view=views.join_group, name='join_group'),
+    path('join_group/', view=views.join_group, name='join_group'),
+    # path('<int:pk>/join_group/', view=views.join_group_each, name='join_group_each'),
+    path('star_ajax/', view=views.star_ajax, name='star_ajax'),
     # path('group_list/', view=views.group_list, name='group_list')
 ]
--- group/views.py
+++ group/views.py
@@ -1,21 +1,25 @@
 import uuid
 import base64
 import codecs
+import json
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
 from .forms import *
 
+## 그룹 메인 페이지
+
 # 나의 그룹
 def group_home(request):
-    # user = get_object_or_404(User)  # 현재 접속한 사용자
+    user = request.user  # 현재 접속한 사용자
 
     # 해당 유저의 그룹 리스트
-    # groups = Group.objects.filter(members__nickname__contains=user.nickname)  
-    groups = Group.objects.all()
+    groups = Group.objects.filter(members__nickname__contains=user.nickname)
+    # groups = Group.objects.all()
     members = User.objects.filter(groups__name__contains=groups)
 
     ### user의 닉네임이랑 같은 경우에 처리해야 하는 부분 이후 추가
-    # member = Participation.objects.all()   # 해당 그룹에 가입한 멤버 리스트
     member_cnt = groups.count()  
     # member_cnt = len(members)  # 그룹의 총 멤버 수
 
@@ -23,10 +27,12 @@ def group_home(request):
 
     return render(request, template_name='group/group_home.html', context=ctx)
 
+
+## 그룹 CRUD
+
 # 그룹 생성
 def group_create(request):
-    user = get_object_or_404(User)
-    # user = get_object_or_404(User, user=request.nickname)
+    user = request.user
     if request.method == 'POST':
         form = GroupForm(request.POST)
         if form.is_valid():
@@ -39,7 +45,7 @@ def group_create(request):
     else:
         form = GroupForm()
         users = User.objects.all()
-        # users = User.objects.all().exclude(user=request.nickname)
+        # users = User.objects.exclude(user)
         # form.fields['maker'].queryset = users
         ctx = { 'form': form }
         
@@ -73,6 +79,9 @@ def group_delete(request, pk):
 
     return redirect('group_home')
 
+
+## 초대 코드 
+
 # 초대 코드 발급
 def get_invite_code(length=6):
     return base64.urlsafe_b64encode(
@@ -90,15 +99,56 @@ def create_code(request, pk):
     return render(request, template_name='group/create_code.html', context=ctx)
 
 # 초대 코드 입력(from 메인 페이지)
-# def join_group(request):
-#     group = get_object_or_404(Group)
+def join_group(request):    
+    user = request.user
+    input_code = request.GET.get('code')
+
+    mygroup = list(Group.objects.filter(members__nickname__contains=user))
+    group = get_object_or_404(Group, code=input_code)
+    print(group)
+    # 예외처리 or 조건문
+    if group in mygroup:
+        message = "이미 가입된 그룹입니다."
+        ctx = { 'message': message }
+
+        return render(request, template_name='group/join_group.html', context=ctx)
+
+    else:
+        if group == ' ':
+            message = "존재하지 않는 코드입니다."
+            ctx = { 'message': message }
+
+            return render(request, template_name='group/join_group.html', context=ctx)
+        else:
+            group.members.add(user)  # 방장도 그룹의 멤버로 추가
+            group.save()
+
+            return redirect('group_home')
+
+# # 초대 코드 입력(from 그룹 상세 페이지) -> 비공개 그룹에서는 필요가 없네..ㅋㅋ
+# def join_group_each(request, pk):
+#     group = get_object_or_404(Group, pk=pk)
+
+
+
+
+    # if gs'
+    #     groups.members.add(user)  # 방장도 그룹의 멤버로 추가
+    #     group.save()
+
+    #     return redirect('group_home')
+    # else:
+    #     result = 'fail'
+    # ctx = { 'result': result }
+
 
     
 
 # 그룹 상세 페이지
 def group_detail(request, pk):
     groups = get_object_or_404(Group, pk=pk)
-    members = User.objects.filter(groups__name__contains=groups)
+    members = User.objects.all()
+    print(members)
 
     ctx = { 'group': groups, 'members': members }
 
@@ -110,3 +160,21 @@ def group_list(request):
     ctx = { 'groups': group }
 
     return render(request, template_name='group/group_list.html', context=ctx)
+
+
+## Ajax
+# 내 그룹 - 찜 기능 ajax
+@csrf_exempt
+def star_ajax(request):
+    req = json.loads(request.body)
+    group_id = req['id']
+    group = get_object_or_404(Group, id=group_id)
+    
+    if (group.is_star):
+        group.is_star = False
+    else:
+        group.is_star = True
+
+    group.save()
+
+    return JsonResponse({ 'id': group_id })
