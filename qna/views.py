fbe4021454f4a92703f39ebf04836150b8c41714
Eunji Gil <geunji0815@gmail.com>
2022-02-06 00:02:53 +0900

    [FEAT] 게시글 작성 ( 중)

--- qna/views.py
+++ qna/views.py
@@ -1,14 +1,50 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import *
+from django.contrib import messages
 from django.db.models import Q
 
-# Create your views here.
 def question_list(request):
     questions = Question.objects.all()
     ctx = {'questions': questions}
 
     return render(request, 'qna/question_list.html', context=ctx)
+
+# # 검색 조건
+# def search_condition(self):
+#     search_keyword = self.request.GET.get('query', '')
+#     search_type = self.request.GET.get('type', '')
+#     questions = Question.objects.order_by('-id')
+
+#     if search_keyword :
+#         if len(search_keyword) > 1:
+#             if search_type == 'all':
+#                 search_questions = questions.filter(Q (title__icontains=search_keyword) | Q (content__icontains=search_keyword) | Q (writer__user_id__icontains=search_keyword))
+#             elif search_type == 'title_content':    # 제목, 내용
+#                 search_questions = questions.filter(Q (title__icontains=search_keyword) | Q (content__icontains=search_keyword))
+#             elif search_type == 'title':    # 제목
+#                 search_questions = questions.filter(title__icontains=search_keyword)    
+#             elif search_type == 'content':  # 내용
+#                 search_questions = questions.filter(content__icontains=search_keyword)    
+#             elif search_type == 'writer':   # 작성자
+#                 search_questions = questions.filter(writer__user_id__icontains=search_keyword)
+            
+#             return search_questions
+
+#         else:
+#             messages.error(self.request, '검색어는 2글자 이상 입력해주세요.')
+
+#     return questions
+
+# def search_result(self, **kwargs):
+#     search_keyword = self.request.GET.get('search', '')
+#     search_type = self.request.GET.get('type', '')
+
+#     if len(search_keyword) > 1:
+#         context['search'] = search_keyword
+#         context['type'] = search_type
+
+#     return context
 
 def search_result(request):
     if 'search' in request.GET:
@@ -23,10 +59,12 @@ def search_result(request):
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST, request.FILES)
+
         if form.is_valid():
             question = form.save()
             return redirect('qna:question_list')
+        print("print")         
     else:
         form = QuestionForm()
-    ctx = {'form': form}
-    return render(request, 'qna/question_create.html', context=ctx)
+        ctx = {'form': form}
+        return render(request, 'qna/question_create.html', context=ctx)
