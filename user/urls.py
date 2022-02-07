0bd2ee81c1d3c6b55d4e8561d05fc0238bc527d6
PARK CHANYOUNG <pcy1203@snu.ac.kr>
2022-02-07 15:51:22 +0900

    [FEAT] user mypage revise, password revise

--- user/urls.py
+++ user/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
   path('signup/', views.sign_up, name="signup"),
   path('activate/<uid64>/<token>/', views.activate, name="activate"),
   path('mypage/', views.my_page, name="mypage"),
+  path('mypage/revise/', views.my_page_revise, name="mypage_revise"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import AnonymousUser
 from django.views import View
-from .forms import LoginForm, SignupForm
+from .forms import LoginForm, SignupForm, MypageReviseForm
 from .models import *
 from qna.models import Question, Answer
 from django.contrib import auth
+from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.hashers import check_password
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
@@ -14,6 +17,7 @@ from .tokens import user_activation_token
 from django.utils.encoding import force_bytes, force_str
 import re
 
+# Main
 def main(request):
     return render(request, 'user/main.html')
 
@@ -40,25 +44,43 @@ def log_out(request):
     logout(request)
     return redirect('user:main')
 
+# Validation Check (Sign Up / My Page Revise)
+def validation_check(email, current_password, new_password1, new_password2, birth, command):
+    error = ''
+
+    if 'signup' in command:
+        if not re.compile('^[a-zA-Z0-9]+@[a-zA-Z0-9.]+$').match(email):  # 예외1-1) 잘못된 이메일 형식
+            error += '올바른 이메일 주소를 입력해주세요.\n'
+        elif User.objects.filter(email = email):  # 예외1-2) 이미 가입된 유저
+            error += '이미 가입된 유저입니다.\n'
+
+    if 'password_change' in command:
+        if not check_password(current_password, User.objects.get(email = email).password):  # 예외2-1) 현재 비밀번호 오류
+            error += '현재 비밀번호가 일치하지 않습니다.\n'
+
+    if 'signup' in command or 'password_change' in command:
+        if not re.compile('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d$@$!%*#?&]{8,}$').match(new_password1):  # 예외3-1) 잘못된 비밀번호 형식
+            error += '비밀번호는 영문자와 숫자를 혼합하여 8자리 이상으로 만들어주세요. (사용 가능 특수 기호 : $@$!%*#?&)\n'
+        elif new_password1 != new_password2:  # 예외3-2) 비밀번호 불일치
+            error += '비밀번호가 일치하지 않습니다.\n'
+
+    if not re.compile('^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$').match(birth):  # 예외4) 잘못된 생년월일 형식
+        error += '생년월일을 2022-02-22 형식으로 입력해주세요.\n'
+    
+    return error
+
 # Sign Up
 def sign_up(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-        sign_up_error = ''
-
-        if not re.compile('^[a-zA-Z0-9]+@[a-zA-Z0-9.]+$').match(request.POST['email']):  # 예외1-1) 잘못된 이메일 형식
-            sign_up_error += '올바른 이메일 주소를 입력해주세요.\n'
-        elif User.objects.filter(email = request.POST['email']):  # 예외1-2) 이미 가입된 유저
-            sign_up_error += '이미 가입된 유저입니다.\n'
-            
-        if not re.compile('^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d$@$!%*#?&]{8,}$').match(request.POST['password1']):  # 예외2-1) 잘못된 비밀번호 형식
-            sign_up_error += '비밀번호는 영문자와 숫자를 혼합하여 8자리 이상으로 만들어주세요. (사용 가능 특수 기호 : $@$!%*#?&)\n'
-        elif request.POST['password1'] != request.POST['password2']:  # 예외2-2) 비밀번호 불일치
-            sign_up_error += '비밀번호가 일치하지 않습니다.\n'
-
-        if not re.compile('[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]').match(request.POST['birth']):  # 예외3) 잘못된 생년월일 형식
-            sign_up_error += '생년월일을 2022-02-22 형식으로 입력해주세요.\n'
-
+        sign_up_error = validation_check(
+            request.POST['email'],
+            '',
+            request.POST['password1'],
+            request.POST['password2'],
+            request.POST['birth'],
+            ['signup']
+        )
         if not sign_up_error:  # 오류가 없는 경우 유저 생성
             user = User.objects.create_user(
                 username = request.POST['email'],
@@ -108,8 +130,52 @@ def activate(request, uid64, token):
 # My Page
 def my_page(request):
     user = request.user
+    if user == AnonymousUser():
+        return redirect('user:login')
     rewards = GetReward.objects.filter(user_id = user).order_by('-get_date')[:5]
     questions = Question.objects.filter(user = user).order_by('-updated_at')[:5]
     answers = Answer.objects.filter(user = user).order_by('-updated_at')[:5]
     ctx = {'user': user, 'rewards': rewards, 'questions': questions, 'answers': answers}
-    return render(request, template_name='user/mypage.html', context = ctx)
+    return render(request, template_name = 'user/mypage.html', context = ctx)
+
+# My Page Revise
+def my_page_revise(request):
+    user = request.user
+    if user == AnonymousUser():
+        return redirect('user:login')
+    if request.method == 'POST':
+        form = MypageReviseForm(request.POST)
+        command = ['mypage_revise']
+        if request.POST['current_password'] or request.POST['new_password1'] or request.POST['new_password2']:
+            command += ['password_change']
+        if request.FILES.get('img'):
+            command += ['image_change']
+        revise_error = validation_check(
+            user.email,
+            request.POST['current_password'],
+            request.POST['new_password1'],
+            request.POST['new_password2'],
+            request.POST['birth'],
+            command
+        )
+        if not revise_error:
+            updated = '프로필이 성공적으로 수정되었습니다.'
+            if 'password_change' in command:
+                user.set_password(request.POST['new_password1'])
+                updated += ' (비밀번호가 성공적으로 변경되었습니다.)'
+            if 'image_change' in command:
+                user.img = request.FILES.get('img')
+            user.nickname = request.POST['nickname']
+            user.birth = request.POST['birth']
+            user.introduction = request.POST['introduction']
+            user.job = request.POST['job']
+            user.save()
+            login(request, user)
+            messages.success(request, updated)
+            return redirect('user:mypage')
+        ctx = {'user': user, 'form': form, 'revise_error': revise_error}
+        return render(request, template_name = 'user/mypage_revise.html', context = ctx)
+    else:
+        form = MypageReviseForm(instance = user)
+        ctx = {'user': user, 'form': form}
+        return render(request, template_name = 'user/mypage_revise.html', context = ctx)
