2eebca3a5d358c270a9873ef74395b8477849f1c
박찬영 <pcy1203@snu.ac.kr>
2022-02-21 22:53:42 +0900

    [FIX] user mypage fix

--- static/js/user_profile_redirect.js
+++ static/js/user_profile_redirect.js
@@ -1,18 +1,16 @@
 const redirectButton = document.querySelector("#profile-redirect");
 currentURL = window.location.href.split("/")
-if (currentURL[currentURL.length - 3] != "mypage") {
+if (currentURL[currentURL.length - 4] != "mypage") {
   redirectButton.innerHTML = "유저페이지로 돌아가기";
-  try {
-    const goQnaButton = document.querySelector("#profile-go-qna");
+  const goQnaButton = document.querySelector("#profile-go-qna");
+  if (goQnaButton != null) {
     goQnaButton.remove();
-  } catch (e) {
-    
   }
+} else {
+  var redirectURL = "";
+  for (index = 0; index < currentURL.length - 2; index++) {
+    redirectURL = redirectURL + currentURL[index];
+    redirectURL = redirectURL + "/";
+  }
+  redirectButton.href = redirectURL;
 }
-var redirectURL = "";
-for (index = 0; index < currentURL.length - 2; index++) {
-  redirectURL = redirectURL + currentURL[index];
-  redirectURL = redirectURL + "/";
-}
-
-redirectButton.href = redirectURL;
