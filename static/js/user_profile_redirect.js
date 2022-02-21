14b6f2bda00506530d2faf659b1d4bdcf5fd8a07
박찬영 <pcy1203@snu.ac.kr>
2022-02-21 23:02:45 +0900

    [FIX] user mypage redirection fix

--- static/js/user_profile_redirect.js
+++ static/js/user_profile_redirect.js
@@ -1,6 +1,7 @@
 const redirectButton = document.querySelector("#profile-redirect");
 currentURL = window.location.href.split("/")
-if (currentURL[currentURL.length - 4] != "mypage") {
+if ((currentURL[currentURL.length - 4] != "mypage" || currentURL[currentURL.length - 3] != "reward")
+  && currentURL[currentURL.length - 3] != "mypage") {
   redirectButton.innerHTML = "유저페이지로 돌아가기";
   const goQnaButton = document.querySelector("#profile-go-qna");
   if (goQnaButton != null) {
